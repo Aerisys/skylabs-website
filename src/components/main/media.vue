b8e925f3010cb810ea52b73d9f62b5eee149b905
GaetanccgYnov <148949692+GaetanccgYnov@users.noreply.github.com>
2024-12-10 16:14:13 +0100

    Finish init & firsts main components (#5)
    Co-authored-by: Gaetan Chollet <gaetan.chollet@jetpulp.fr>

---
+++ src/components/main/media.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+    src: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        default: 'image', // "image" ou "video"
+        validator: (value) => [
+            'image',
+            'video'
+        ].includes(value)
+    }
+});
+</script>
+
+<template>
+    <div class="flex-1 w-full h-full overflow-hidden rounded-md bg-gray-200">
+        <!-- Affichage de l'image -->
+        <img
+            v-if="type === 'image'"
+            :src="src"
+            alt="Media content"
+            class="w-full h-full object-cover"
+        />
+
+        <!-- Affichage de la vidéo -->
+        <video
+            v-else
+            :src="src"
+            controls
+            class="w-full h-full object-cover"
+        ></video>
+    </div>
+</template>
