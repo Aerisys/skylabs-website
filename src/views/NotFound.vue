761e22e4bc38a7e4abb628646de47dae5551ba77
GaetanccgYnov <gaetan.chollet@ynov.com>
2025-04-30 12:38:10 +0200

    add 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-center p-52">
+    <h1 class="text-4xl font-bold text-red-500">404</h1>
+    <p class="text-lg text-white">La page que vous recherchez n'existe pas.</p>
+    <router-link to="/" class="text-blue-500 hover:underline">Retour à l'accueil</router-link>
+  </div>
+</template>
